7d38f180a2dd1e350300c84b9052875af5380318
felixdanie21 <felixminato12@gmail.com>
2024-10-17 12:56:13 +0700

    baru

---
+++ src/main/java/ukrim/co/id/serversideukrim/BackendjavaApplication.java
@@ -0,0 +1,13 @@
+package ukrim.co.id.serversideukrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendjavaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ukrim/co/id/serversideukrim/model/Region.java
@@ -0,0 +1,6 @@
+package ukrim.co.id.serversideukrim.model;
+
+public class Region {
+    private long id;
+    private String name;
+}
